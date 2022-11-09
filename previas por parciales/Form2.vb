7cd6a9228a34bc2f3f1751bfd7a8bac16fbe0350
tamagoru <marushannnn@outlook.com>
2022-11-09 18:27:09 -0300

    falta la info en los combobox

--- previas por parciales/Form2.vb
+++ previas por parciales/Form2.vb
@@ -10,26 +10,41 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If ComboBox1.Visible Then
-
-        End If
-
         Dim atributos As String
+        Dim x As Boolean = False
 
-        If GroupBox1.Visible Then
-            atributos = "VALUES ('" + txnombre.Text.Trim + "')"
+        If ComboBox1.Visible Then
+            If txnombre.Text.Trim = String.Empty Then
+                MsgBox("Error. Espacios en blanco.")
+            Else
+                atributos = "VALUES ('" + txnombre.Text.Trim + "')"
+
+                x = True
+            End If
         Else
-            atributos = "VALUES ('" + txtitulo.Text.Trim + "', '" +
-                        ComboBox1.Text + "', '" +
-                        ComboBox2.Text + "', '" +
-                        MaskedTextBox1.Text + "')"
+            If txtitulo.Text.Trim = String.Empty Or
+                ComboBox1.Text = String.Empty Or
+                ComboBox2.Text = String.Empty Or
+                MaskedTextBox1.MaskCompleted = False Then
+
+                MsgBox("Error. Espacios en blanco.")
+            Else
+                atributos = "VALUES ('" + txtitulo.Text.Trim + "', '" +
+                            ComboBox1.Text + "', '" +
+                            ComboBox2.Text + "', '" +
+                            MaskedTextBox1.Text + "')"
+
+                x = True
+            End If
         End If
 
-        Dim comando As String
+        If x Then
+            Dim comando As String
 
-        comando = "Insert into " + Form1.ComboBox1.Text + " " + atributos
+            comando = "Insert into " + Form1.ComboBox1.Text + " " + atributos
 
-        conexion.execute(comando)
-        Form1.dgvrefresh()
+            conexion.execute(comando)
+            Form1.dgvrefresh()
+        End If
     End Sub
 End Class
