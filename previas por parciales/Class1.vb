657911d251a1f4049a3fb0a29d12db2e01599de8
tamagoru <marushannnn@outlook.com>
2022-11-09 16:04:06 -0300

    Add project files.

---
+++ previas por parciales/Class1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Class1
+    Public conn As New SqlConnection
+    Public cmd As New SqlCommand
+    Public dataadapter As New SqlDataAdapter
+    Public dataser As New DataSet
+    Public connerror As String
+
+    Sub New()
+        Try
+            conn.ConnectionString = ""
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ previas por parciales/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Dim conexion As New Class1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
